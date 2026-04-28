01ea497ffe801e7931b2eda160c54f12cf91bbd9
Majkelelek <michal469@gmail.com>
2026-04-28 20:55:14 +0200

    fix: Update title in index.html and enhance web.config for static content handling

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // DODAJ TO: Wymusza, aby ścieżki do assets zaczynały się od / (np. /assets/...)
   envDir: '../', // Wskazuje, że .env jest jeden poziom wyżej
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // Czyści folder przed nowym buildem
+  }
 })
